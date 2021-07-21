8105d46c1dee5346aa91cf24055329a99da6f49f
damien-deathstalker <bekoekofi@gmail.com>
2021-07-21 09:21:02 +0000

    Modified Allowed Hosts

--- Library/settings.py
+++ Library/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=s2yf@^6fmap))^wvkk1+7xowxyapzuw#&epw(7f_239d5nz_2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
