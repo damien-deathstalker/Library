d517cd6a343857ec4a42238c127b91b5608301c7
damien-deathstalker <bekoekofi@gmail.com>
2021-07-22 13:03:57 +0000

    Modified the admin in books and added some QOL in the models

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,52 @@
 from django.contrib import admin
-from .models import Book, Chapter
+from .models import Book, Chapter, Comment
+from django.contrib.auth.models import Group
 # Register your models here.
-admin.site.register(Book)
-admin.site.register(Chapter)
+
+class adminBook(admin.ModelAdmin):
+	list_display = ['cover', 'name', 'description']
+	list_display_links = ['name']
+	
+	def description(self, obj):
+		return obj.blurb
+	
+	def cover(self, obj):
+		return obj.get_cover_image(height=140, width=140)
+
+class adminComment(admin.ModelAdmin):
+	list_display = ['name', 'comment', 'chapter_name', 'book_name']
+	list_display_links = ['comment']
+	list_filter = ['chapter_fk__book_fk__name']
+	search_fields = ['name', 'chapter_fk__book_fk__name']
+
+	def chapter_name(self, obj):
+		return obj.chapter_fk.title
+	
+	def book_name(self, obj):
+		return obj.chapter_fk.book_fk.name
+	
+	def comment(self, obj):
+		return obj.comment_post
+	
+	def has_add_permission(self, request) -> bool:
+		return False
+
+class adminChapter(admin.ModelAdmin):
+	list_display = ['book_name', 'title']
+	list_filter = ['book_fk__name']
+	ordering = ['book_fk', 'id']
+	list_display_links = ['title']
+
+	def book_name(self, obj):
+		return obj.book_fk.name
+
+admin.site.register(Book, adminBook)
+admin.site.register(Chapter, adminChapter)
+admin.site.register(Comment, adminComment)
+
+# Unregister your models here.
+admin.site.unregister(Group)
+
+admin.site.site_title = "Damien's Bookshelf - Administration Site"
+admin.site.site_header = "Damien's Bookshelf - Administration"
+admin.site.index_title  =  "Damien's Bookshelf - Administration"
--- books/models.py
+++ books/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.base import Model
+from django.utils.safestring import mark_safe
 
 # Create your models here.
 class Book(models.Model):
@@ -12,6 +12,18 @@ class Book(models.Model):
 
 	def get_chapters(self):
 		return Chapter.objects.filter(book_fk=self)
+	
+	def get_cover_image(self, **kwargs):
+		height = kwargs.get('height', None)
+		width = kwargs.get('width', None)
+		if not all([height, width]):
+			height = 150
+			width = 150
+		return mark_safe(f'<img style="max-width: {width}px; max-height: {height}px;" src="/media/{self.cover_image}/">')
+	
+	class Meta:
+		verbose_name = 'Book'
+		verbose_name_plural = 'Books'
 
 class Chapter(models.Model):
 	book_fk = models.ForeignKey(Book, on_delete=models.CASCADE)
@@ -21,7 +33,15 @@ class Chapter(models.Model):
 	def __str__(self):
 		return f'{self.title} of {self.book_fk}'
 
+	class Meta:
+		verbose_name = 'Book Chapter'
+		verbose_name_plural = 'Book Chapters'
+
 class Comment(models.Model):
 	chapter_fk = models.ForeignKey(Chapter, on_delete=models.CASCADE)
 	name = models.CharField(max_length=50)
 	comment_post = models.TextField()
+
+	class Meta:
+		verbose_name = 'Comment'
+		verbose_name_plural = 'Comments'
