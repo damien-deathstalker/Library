e40f6aadee7aa8b3f22297daead3627378bacc1a
damien-deathstalker <bekoekofi@gmail.com>
2022-11-24 16:46:47 +0000

    API subscription service

--- api/views.py
+++ api/views.py
@@ -3,14 +3,15 @@ from django.http import JsonResponse
 import requests
 
 def exchangeOne(request):
-    exchange_request = requests.get("https://exchange.matraining.com/md")
-    if exchange_request.status_code == 200:
-        data = exchange_request
-        MallonExchangeOne.objects.update_or_create(id=1, defaults=dict(
-            market_data = data.json()
-        ))
-        return JsonResponse(data.json)
-    return JsonResponse([])
+    if request.method == "POST":
+        with open("test.txt", "w") as file:
+            file.write(request.POST)
+    elif request.method == "GET":
+        data = []
+        try: data = MallonExchangeOne.objects.get(id=1).market_data
+        except: pass
+        return JsonResponse(data)
+
 
 def exchangeTwo(request):
     exchange_request = requests.get("https://exchange2.matraining.com/md")
