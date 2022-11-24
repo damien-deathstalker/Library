57cd29cdd6af38c068034b6f7118e86d66fd0a7e
damien-deathstalker <bekoekofi@gmail.com>
2022-11-24 16:57:45 +0000

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,16 +1,18 @@
 from .models import MallonExchangeOne, MallonExchangeTwo
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 import requests
 
+@csrf_exempt
 def exchangeOne(request):
     if request.method == "POST":
-        with open("test.txt", "w") as file:
+        with open("/home/dadeathstalker/Library/test.txt", "w") as file:
             file.write(request.POST)
     elif request.method == "GET":
-        data = []
+        data = [{}]
         try: data = MallonExchangeOne.objects.get(id=1).market_data
         except: pass
-        return JsonResponse(data)
+        return JsonResponse(data, safe=False)
 
 
 def exchangeTwo(request):
