153221dde31e9ecd68ecfb2d3a1312ad01810ae8
damien-deathstalker <bekoekofi@gmail.com>
2022-11-29 10:33:28 +0000

    API Edit

--- api/views.py
+++ api/views.py
@@ -1,24 +1,63 @@
 from .models import MallonExchangeOne, MallonExchangeTwo
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-import requests
+import requests, json
+
+
+class Price:
+    def __init__(self, bid, ask) -> None:
+        self.buy = bid
+        self.sell = ask
+    
+    def __str__(self) -> str:
+        return f"{self.buy} - {self.sell}"
+    
+    def __repr__(self) -> str:
+        return str(self)
+
+class MarketData:
+    def __init__(self, trade_data:dict) -> None:
+        self.SELL_LIMIT = trade_data.get("SELL_LIMIT")
+        self.ASK_PRICE = trade_data.get("ASK_PRICE")
+        self.BUY_LIMIT = trade_data.get("BUY_LIMIT")
+        self.MAX_PRICE_SHIFT = trade_data.get("MAX_PRICE_SHIFT")
+        self.LAST_TRADED_PRICE = trade_data.get("LAST_TRADED_PRICE")
+        self.TICKER = trade_data.get("TICKER")
+        self.BID_PRICE = trade_data.get("BID_PRICE")
+        self.PRICE = Price(trade_data.get("BID_PRICE"), trade_data.get("ASK_PRICE"))
+    
+    def __str__(self) -> str:
+        return f"{self.TICKER} ({self.BID_PRICE}/{self.ASK_PRICE})"
+
+    def __repr__(self) -> str:
+        return str(self)
+    
+    @property
+    def as_dict(self) -> dict:
+        data = {}
+        data[self.TICKER] = str(self.PRICE)
+        return data
+
+        
 
 @csrf_exempt
 def exchangeOne(request):
-    data = [{}]
+    data = {}
     if request.method == "POST":
-        with open("/home/dadeathstalker/Library/test.txt", "w") as file:
-            file.write("Request Stuff")
-            file.write("\n")
-            file.write(str(request.text))
-            file.write("\n")
-            file.write(str(request.body))
-            file.write("\n")
-            file.write(str(request))
+        data = json.loads(request.body)
+        MallonExchangeOne.objects.update_or_create(id=1, defaults=dict(
+            market_data = data
+        ))
     elif request.method == "GET":
-        try: data = MallonExchangeOne.objects.get(id=1).market_data
-        except: pass
-    return JsonResponse(data, safe=False)
+        try: 
+            exchange = MallonExchangeOne.objects.get(id=1)
+            market_data = exchange.market_data
+            for md in market_data:
+                md_obj = MarketData(md)
+                data.update(md_obj.as_dict)
+                # data.append(md_obj.as_dict)
+        except Exception as e: print(e)
+    return JsonResponse(data)
 
 
 def exchangeTwo(request):
